e94e1c4fb1719bfd8e84bd5a8060feb1c1bab8a5
JiYou <jiyou09@gmail.com>
2016-05-27 21:52:50 +0800

    update mysql install script.
    Signed-off-by: JiYou <jiyou09@gmail.com>

--- lession3/mysql-inst.sh
+++ lession3/mysql-inst.sh
@@ -10,6 +10,15 @@ if [[ `cat /etc/mysql/my.cnf | grep "0.0.0.0" | wc -l` -eq 0 ]]; then
 fi
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/my.cnf
 service mysql restart
+
+for n in `hostname -I`; do
+        mysql -uroot -p$MYSQL_ROOT_PASSWORD  -e "use mysql; insert into user (Host,User,Password) values ('$n','root',Password('$MYSQL_ROOT_PASSWORD'));"
+done
+
+host_name=`hostname -s`
+mysql -uroot -p$MYSQL_ROOT_PASSWORD  -e "use mysql; insert into user (Host,User,Password) values ('$host_name','root',Password('$MYSQL_ROOT_PASSWORD'));"
+
+
 mysql -uroot -p$MYSQL_ROOT_PASSWORD  -e "use mysql; delete from user where user=''; flush privileges;"
 mysql -uroot -p$MYSQL_ROOT_PASSWORD  -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;"
 mysql -uroot -p$MYSQL_ROOT_PASSWORD  -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD'  WITH GRANT OPTION; FLUSH PRIVILEGES;"
